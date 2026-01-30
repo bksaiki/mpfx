29926defc25803cd6ef2ab5c3a8df5e3043e8c8e
Brett Saiki <bksaiki@gmail.com>
2026-01-29 17:18:23 -0800

    move softfloat and floppyfloat engines to source

--- include/mpfx/engine_ff.hpp
+++ include/mpfx/engine_ff.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <floppy_float.h>
-#include <vfpu.h>
-
 #include "types.hpp"
 
 namespace mpfx {
@@ -13,164 +10,37 @@ namespace engine_ff {
 ///
 /// Ensures the result has at least `p` bits of precision.
 /// Otherwise, an exception is thrown.
-inline double add(double x, double y, prec_t p) {
-    // double-precision only guarantees 53 bits of precision
-    MPFX_DEBUG_ASSERT(
-        p <= 53,
-        "add: requested precision exceeds double-precision capability"
-    );
-
-    // initialize FloppyFloat instance
-    static FloppyFloat ff;
-    ff.rounding_mode = Vfpu::kRoundTowardZero;
-
-    // Perform round-to-zero addition with round-to-odd fix-up
-    double z = ff.Add(x, y);
-    if (ff.inexact) {
-        uint64_t b = std::bit_cast<uint64_t>(z);
-        b |= 1; // set LSB
-        z = std::bit_cast<double>(b);
-        ff.inexact = false;
-    }
-
-    return z;
-}
+double add(double x, double y, prec_t p);
 
 /// @brief Computes `x - y` using FloppyFloat.
 ///
 /// Ensures the result has at least `p` bits of precision.
 /// Otherwise, an exception is thrown.
-inline double sub(double x, double y, prec_t p) {
-    // double-precision only guarantees 53 bits of precision
-    MPFX_DEBUG_ASSERT(
-        p <= 53,
-        "sub: requested precision exceeds double-precision capability"
-    );
-
-    // initialize FloppyFloat instance
-    static FloppyFloat ff;
-    ff.rounding_mode = Vfpu::kRoundTowardZero;
-
-    // Perform round-to-zero subtraction with round-to-odd fix-up
-    double z = ff.Sub(x, y);
-    if (ff.inexact) {
-        uint64_t b = std::bit_cast<uint64_t>(z);
-        b |= 1; // set LSB
-        z = std::bit_cast<double>(b);
-        ff.inexact = false;
-    }
-
-    return z;
-}
+double sub(double x, double y, prec_t p);
 
 /// @brief Computes `x * y` using FloppyFloat.
 ///
 /// Ensures the result has at least `p` bits of precision.
 /// Otherwise, an exception is thrown.
-inline double mul(double x, double y, prec_t p) {
-    // double-precision only guarantees 53 bits of precision
-    MPFX_DEBUG_ASSERT(
-        p <= 53,
-        "mul: requested precision exceeds double-precision capability"
-    );
-
-    // initialize FloppyFloat instance
-    static FloppyFloat ff;
-    ff.rounding_mode = Vfpu::kRoundTowardZero;
-
-    // Perform round-to-zero multiplication with round-to-odd fix-up
-    double z = ff.Mul(x, y);
-    if (ff.inexact) {
-        uint64_t b = std::bit_cast<uint64_t>(z);
-        b |= 1; // set LSB
-        z = std::bit_cast<double>(b);
-        ff.inexact = false;
-    }
-
-    return z;
-}
+double mul(double x, double y, prec_t p);
 
 /// @brief Computes `x / y` using FloppyFloat.
 ///
 /// Ensures the result has at least `p` bits of precision.
 /// Otherwise, an exception is thrown.
-inline double div(double x, double y, prec_t p) {
-    // double-precision only guarantees 53 bits of precision
-    MPFX_DEBUG_ASSERT(
-        p <= 53,
-        "div: requested precision exceeds double-precision capability"
-    );
-
-    // initialize FloppyFloat instance
-    static FloppyFloat ff;
-    ff.rounding_mode = Vfpu::kRoundTowardZero;
-
-    // Perform round-to-zero division with round-to-odd fix-up
-    double z = ff.Div(x, y);
-    if (ff.inexact) {
-        uint64_t b = std::bit_cast<uint64_t>(z);
-        b |= 1; // set LSB
-        z = std::bit_cast<double>(b);
-        ff.inexact = false;
-    }
-
-    return z;
-}
+double div(double x, double y, prec_t p);
 
 /// @brief Computes `sqrt(x)` using FloppyFloat.
 ///
 /// Ensures the result has at least `p` bits of precision.
 /// Otherwise, an exception is thrown.
-inline double sqrt(double x, prec_t p) {
-    // double-precision only guarantees 53 bits of precision
-    MPFX_DEBUG_ASSERT(
-        p <= 53,
-        "sqrt: requested precision exceeds double-precision capability"
-    );
-
-    // initialize FloppyFloat instance
-    static FloppyFloat ff;
-    ff.rounding_mode = Vfpu::kRoundTowardZero;
-
-    // Perform round-to-zero square root with round-to-odd fix-up
-    double z = ff.Sqrt(x);
-    if (ff.inexact) {
-        uint64_t b = std::bit_cast<uint64_t>(z);
-        b |= 1; // set LSB
-        z = std::bit_cast<double>(b);
-        ff.inexact = false;
-    }
-
-    return z;
-}
+double sqrt(double x, prec_t p);
 
 /// @brief Computes `x * y + z` using FloppyFloat.
 ///
 /// Ensures the result has at least `p` bits of precision.
 /// Otherwise, an exception is thrown.
-inline double fma(double x, double y, double z, prec_t p) {
-    // double-precision only guarantees 53 bits of precision
-    MPFX_DEBUG_ASSERT(
-        p <= 53,
-        "fma: requested precision exceeds double-precision capability"
-    );
-
-    // initialize FloppyFloat instance
-    FloppyFloat ff;
-    ff.rounding_mode = Vfpu::kRoundTowardZero;
-
-    // Perform round-to-zero FMA with round-to-odd fix-up
-    double result = ff.Fma(x, y, z);
-    if (ff.inexact) {
-        uint64_t b = std::bit_cast<uint64_t>(result);
-        b |= 1; // set LSB
-        result = std::bit_cast<double>(b);
-        ff.inexact = false;
-    }
-
-    return result;
-}
-
+double fma(double x, double y, double z, prec_t p);
 
 } // end namespace engine_ff
 
