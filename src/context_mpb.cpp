3f7ee0b80b60b277f4917cfa9514ae2faaad33b5
Brett Saiki <bksaiki@gmail.com>
2026-01-31 18:38:55 -0800

    fix emax

--- src/context_mpb.cpp
+++ src/context_mpb.cpp
@@ -3,11 +3,10 @@
 namespace mpfx {
 
 MPBContext::MPBContext(prec_t prec, exp_t emin, double maxval, RM rm)
-    : Context(prec, emin - static_cast<exp_t>(prec), maxval, rm)
-    , emin_(emin) {
+    : Context(prec, emin - static_cast<exp_t>(prec), maxval, rm), emin_(emin) {
     // Compute emax_ from maxval
     using FP = float_params<double>::params;
-    emax_ = maxval == 0.0 ? FP::EMIN : static_cast<exp_t>(std::ilogb(maxval));
+    emax_ = maxval == 0.0 ? FP::EXPMIN : static_cast<exp_t>(std::ilogb(maxval));
 }
 
 } // namespace mpfx
