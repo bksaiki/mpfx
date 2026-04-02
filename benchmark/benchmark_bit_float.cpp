eea8f78f7eaef0166e71b084bd2ed52ddd08b923
Brett Saiki <bksaiki@gmail.com>
2026-04-02 12:38:01 -0700

    update benchmark

--- benchmark/benchmark_bit_float.cpp
+++ benchmark/benchmark_bit_float.cpp
@@ -7,6 +7,22 @@
 #include <mpfx.hpp>
 
 template <size_t N>
+double benchmark_ref(const std::vector<double>& x_vals) {
+    const auto start = std::chrono::steady_clock::now();
+
+    volatile float result = 0.0;
+    for (size_t i = 0; i < N; i++) {
+        result = static_cast<float>(x_vals[i]);
+    }
+
+    const auto end = std::chrono::steady_clock::now();
+    const auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    (void) result; // prevent unused variable warning
+
+    return static_cast<double>(duration) / N;
+}
+
+template <size_t N>
 double benchmark_double(
     const std::vector<double>& x_vals,
     const std::vector<mpfx::prec_t>& prec_vals,
@@ -76,19 +92,26 @@ void benchmark_rounding_mode(size_t prec_max, mpfx::exp_t n_min, mpfx::exp_t n_m
         n_vals[i] = n_dist(rng);
     }
 
+    std::cout << "Timing static_cast<float>(double)\n";
+    const double avg_time_ref = benchmark_ref<N>(x_vals);
+
     std::cout << "Timing mpfx::round(mpfx::bit_float<double>, ...)\n";
     const double avg_time_double = benchmark_double<N>(x_vals, prec_vals, n_vals, rm);
 
     std::cout << "Timing mpfx::round(mpfx::bit_float<double>, ...)\n";
     const double avg_time_bit_float = benchmark_bit_float<N, rm>(x_vals, prec_vals, n_vals);
+
+    const double speedup_double = avg_time_double / avg_time_ref;
+    const double speedup_bit_float = avg_time_bit_float / avg_time_ref;
     const double speedup = avg_time_double / avg_time_bit_float;
 
     // output results
-    std::cout << "Total operations:        " << N << "\n";
-    std::cout << "Rounding mode:           " << static_cast<int>(rm) << "\n";
-    std::cout << "Average time (double):   " << std::fixed << std::setprecision(2) << avg_time_double << " ns/op\n";
-    std::cout << "Average time (bit_float): " << std::fixed << std::setprecision(2) << avg_time_bit_float << " ns/op\n";
-    std::cout << "Speedup:                " << std::fixed << std::setprecision(2) << speedup << "x\n";
+    std::cout << "Total operations:         " << N << "\n";
+    std::cout << "Rounding mode:            " << static_cast<int>(rm) << "\n";
+    std::cout << "Average time (ref):       " << std::fixed << std::setprecision(2) << avg_time_ref << " ns/op\n";
+    std::cout << "Average time (double):    " << std::fixed << std::setprecision(2) << avg_time_double << " ns/op (" << speedup_double << "x)\n";
+    std::cout << "Average time (bit_float): " << std::fixed << std::setprecision(2) << avg_time_bit_float << " ns/op (" << speedup_bit_float << "x)\n";
+    std::cout << "Speedup:                  " << std::fixed << std::setprecision(2) << speedup << "x\n";
     std::cout << "==========================================================================\n";
 }
 
